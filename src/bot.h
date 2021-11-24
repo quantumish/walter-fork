66248fb52c5244fe8ab814448736da1ce0b48c88
David Freifeld <freifeld.david@gmail.com>
2021-11-24 00:04:23 -0800

    Rough outline of an initial spec
    Co-authored-by: zbuster05 <zakary.saya@gmail.com>

--- src/bot.h
+++ src/bot.h
@@ -1,4 +1,5 @@
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
+#include <Eigen/Dense>
 #include <functional>
 using namespace UNITREE_LEGGED_SDK;
 
@@ -70,16 +71,56 @@ public:
     void execute();
     
     /** 
-     * Adds an instruction to walk forward.
-     * @param distance Distance (in meters) to walk forward.
+     * Adds an instruction to move in the x direction.
+     * @param distance Relative distance (in meters) to walk along x axis.
      * @param velocity Velocity of robot while walking. -0.7 to 1 m/s.
      */
-    void forward(float distance, float velocity);
+    void move_x(float distance, float velocity);
 
+    /** 
+     * Adds an instruction to move in the y direction.
+     * @param distance Relative distance (in meters) to strafe along y axis.
+     * @param velocity Velocity of robot while walking. -0.4 to 0.4 m/s.
+     */
+    void move_y(float distance, float velocity);
+
+    /** 
+     * Adds an instruction to move.
+     * @param position Position to walk to in meters relative to robot.
+     * @param velocity Velocity of robot while walking. -0.4 to 0.4 m/s.
+     */    
+    void move(Eigen::Vector2d position, Eigen::Vector2d velocity);
+
+    /** 
+     * Adds an instruction to move. Rotates first, does no strafing.
+     * @param position Position to walk to in meters relative to robot.
+     * @param velocity Velocity of robot while walking. -0.4 to 0.4 m/s.
+     * @see Bot::move()
+     */
+    void smooth_move(Eigen::Vector2d position, Eigen::Vector2d velocity);
+    
+    void spline_move(); 
+    
     /**
      * Adds an instruction to rotate in place.
      * @param theta Angle to rotate (in rad).
      * @param omega Angular velocity (in rad/s). -2pi/3 to 2pi/3 rad/s.
      */
-    void rotate(float theta, float omega);    
+    void rotate(float theta, float omega);
+
+    /** 
+     * Messes with the lights on the robot, which the manual explicitly says not to do.
+     * You probably shouldn't run this.
+     * @param lights RGB values for each light.
+     */ 
+    [[deprecated("unsafe")]] void set_led(std::vector<Eigen::Vector3i> lights);
+
+    /**
+     * Sets pitch/yaw/roll of bot's body. Pretty useless for now.
+     */
+    [[deprecated]] void set_pyr(Eigen::Vector3d pitch, Eigen::Vector3d yaw, Eigen::Vector3d roll);
+    /** 
+     * Sets height of bot's body from the ground. Pretty useless for now.
+     */
+    [[deprecated]] void set_height(float h, float v);
 };
